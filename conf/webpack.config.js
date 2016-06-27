a79134c6ff4da6feb6cbefe519e62941fcc68e93
Borys Ponomarenko <borysp@backbase.com>
2016-06-27 14:30:50 +0200

    Initial implementation

---
+++ conf/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const externals = require('webpack-node-externals');
+const package = require('../package');
+const debug = process.argv.indexOf('--debug') !== -1;
+
+const config = {
+    debug: debug,
+    context: path.resolve(__dirname, '..'),
+    target: 'node',
+    entry: ['babel-polyfill', './index.js'],
+    output: {
+        path: './dist',
+        libraryTarget: 'commonjs',
+        filename: `${package.name}.js`
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules|dist/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015'],
+                plugins: ['add-module-exports']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json']
+    },
+    plugins: [],
+    externals: [externals()]
+};
+
+if (!debug) {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        sourceMap: false
+    }));
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,4 @@
+export * from './lib/authentication'
+export {default as follows} from './lib/follows'
+export {default as followers} from './lib/followers'
+export {default as media} from './lib/media'
