5fc7127001fb2744372063a2cd3df644d1587196
Borys Ponomarenko <borysp@backbase.com>
2017-05-16 22:37:26 +0200

    feat: Small refactoring

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-export * from './lib/authentication'
-export { default as state } from './lib/utils/state'
-export { default as follows } from './lib/follows'
-export { default as followers } from './lib/followers'
-export { default as media } from './lib/media'
-export { default as user } from './lib/user'
+export * from './lib/authentication';
+export { default as follows } from './lib/follows';
+export { default as followers } from './lib/followers';
+export { default as media } from './lib/media';
+export { default as user } from './lib/user';
